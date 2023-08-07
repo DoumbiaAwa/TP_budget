1e44222166889ed1adc9e14f13909ef345bc2228
touredri <dcompte223@gmail.com>
2023-08-07 12:44:49 +0000

    crud pour depense ajouter

--- src/main/java/com/budget/tp_budget/entity/Depense.java
+++ src/main/java/com/budget/tp_budget/entity/Depense.java
@@ -1,4 +1,30 @@
 package com.budget.tp_budget.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
 public class Depense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (nullable = false)
+    private String titre;
+
+    @Column (nullable = false)
+    private Date date;
+
+    @Column (nullable = false)
+    private Double montant;
+
+    @Column (nullable = false)
+    private String description;
+
+    @Column (nullable = false)
+    private String periode;
+
 }
